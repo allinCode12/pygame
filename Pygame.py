710503daa10fdae2d5ccf789de3f68b016a75297
Patrick <mesinapatrick@gmail.com>
2017-05-26 21:50:30 +0800

    My first commit.

---
+++ Pygame.py
@@ -0,0 +1,13 @@
+#Snake Game
+
+import pygame
+import sys
+import random
+import time
+
+check_errors = pygame.init()
+if check_errors[1] > 0:
+    print("(!) Had {0} initializing errors, exiting....")
+
+
+
